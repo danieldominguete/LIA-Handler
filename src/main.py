3bf1d946991f45a5a4101b999e4f72ecb01c7cc8
Your Name <you@example.com>
2025-02-03 01:10:39 -0300

    fix

--- src/main.py
+++ src/main.py
@@ -73,7 +73,8 @@ app.add_middleware(
 #     return response
 
 
-@app.post("/hello", dependencies=[Depends(verify_api_key)])
+# @app.post("/hello", dependencies=[Depends(verify_api_key)])
+@app.post("/hello")
 async def hello():
     return {"message": "Hello World"}
 
